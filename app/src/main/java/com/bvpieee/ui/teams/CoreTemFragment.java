944385d1b68e6aa0f9792b78b5431e65af7ac0bb
TanishqSehgal7 <tsgl7246@gmail.com>
2020-07-09 19:17:31 +0530

    Core Team Cards Added

--- app/src/main/java/com/bvpieee/ui/teams/CoreTemFragment.java
+++ app/src/main/java/com/bvpieee/ui/teams/CoreTemFragment.java
@@ -2,13 +2,20 @@ package com.bvpieee.ui.teams;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bvpieee.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +28,9 @@ public class CoreTemFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private RecyclerView myRecyclerView;
+    View view;
+    private List<CoreTeamModel> memberCore;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -39,19 +49,39 @@ public class CoreTemFragment extends Fragment {
         return fragment;
     }
 
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        memberCore= new ArrayList<>();
+        memberCore.add(new CoreTeamModel("Kshitij Sidana","ChairpersonB, BVPIEEE",R.drawable.kshitijsidana));
+        memberCore.add(new CoreTeamModel("Himani Negi","Vice Chairperson, BVPIEEE", R.drawable.bvpieee));
+        memberCore.add(new CoreTeamModel("Chetanya Vaid","General Secretary, BVPIEEE",R.drawable.bvpieee));
+        memberCore.add(new CoreTeamModel("Sanjana Deswal","Chief Coordinator, BVPIEEE\nand IAS Chairperson",R.drawable.sanjanadeswal));
+        memberCore.add(new CoreTeamModel("Aaktiti Jain","Joint Secretary, BVPIEEE\nand WIE Vice Chairperon",R.drawable.aakritijain));
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_core_tem, container, false);
+        view=inflater.inflate(R.layout.fragment_core_tem ,container,false);
+        myRecyclerView =(RecyclerView) view.findViewById(R.id.rvCore);
+        RecyclerViewAdapter recyclerViewAdapter=new RecyclerViewAdapter(getContext(),memberCore);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        myRecyclerView.setAdapter(recyclerViewAdapter);
+        return view;
     }
+
 }
