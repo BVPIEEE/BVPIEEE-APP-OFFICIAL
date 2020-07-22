8866dda49bd052c7fd9a1363586a82c5ba4ecfee
saksham0804 <saksham.0804@gmail.com>
2020-07-22 12:42:40 +0530

    social profiles added for chapters

--- app/src/main/java/com/bvpieee/ui/chapters/PlaceholderFragment.java
+++ app/src/main/java/com/bvpieee/ui/chapters/PlaceholderFragment.java
@@ -1,14 +1,20 @@
 package com.bvpieee.ui.chapters;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -37,9 +43,12 @@ public class PlaceholderFragment extends Fragment {
     private TextView title, placeholder;
     private CardView social, socialExpanded, aboutLayout,  eventLayout;
     private RelativeLayout white;
+    ImageButton instagram, facebook, website;
     private Button close;
     private int[] imageResource = {R.drawable.raspp, R.drawable.cspp, R.drawable.iaspp, R.drawable.wiepp, R.drawable.hknpp};
     private int[] sigImageResource = {R.drawable.codex, R.drawable.drishti, R.drawable.robotics, R.drawable.quiz, R.drawable.entrepr, R.drawable.gamma};
+    private String[] fbIds = {"357306677679810","1201811083175429","483730548444337","266525677321638","125463600824133"};
+    private String[] websites = {"https://bvpieeend.com/pages/societies_chapters.html","https://bvpieeend.com/pages/societies_chapters.html","https://bvpieeend.com/pages/societies_chapters.html","https://bvpieeend.com/pages/wie.html","https://bvpieeend.com/pages/societies_chapters.html"};
     Context context;
    String[] arrayList;
 //    private String[] titles = {"RAS", "IAS", "CS", "WIE", "HKN"};
@@ -55,7 +64,7 @@ public class PlaceholderFragment extends Fragment {
             Bundle savedInstanceState) {
         final View root;
         if (chapter.equals("chapter"))
-            root = inflater.inflate(R.layout.fragment_chapter, container, false);
+            root = inflater.inflate(R.layout.fragment_chapter1, container, false);
         else
             root = inflater.inflate(R.layout.fragment_sig, container, false);
         arrayList = context.getResources().getStringArray(R.array.chapter_description);
@@ -64,9 +73,13 @@ public class PlaceholderFragment extends Fragment {
         imageView = root.findViewById(R.id.chapterBanner);
         title = root.findViewById(R.id.textView2);
         social = root.findViewById(R.id.socialLayout);
-        socialExpanded = root.findViewById(R.id.socialLayoutExpanded);
-        close = root.findViewById(R.id.close);
+//        socialExpanded = root.findViewById(R.id.socialLayoutExpanded);
+//        close = root.findViewById(R.id.close);
+        instagram = root.findViewById(R.id.instagram);
+        facebook = root.findViewById(R.id.facebook);
+        website = root.findViewById(R.id.website);
         aboutLayout = root.findViewById(R.id.aboutLayout);
+
 //        white = root.findViewById(R.id.whiteBack);
 
        final AlertDialog.Builder builder = new MaterialAlertDialogBuilder(context);
@@ -83,17 +96,37 @@ public class PlaceholderFragment extends Fragment {
             @Override
             public void onClick(View v) {
 //                white.setVisibility(View.VISIBLE);
-                socialExpanded.setVisibility(View.VISIBLE);
-                close.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        socialExpanded.setVisibility(View.GONE);
-//                        white.setVisibility(View.INVISIBLE);
-                    }
-                });
+//                socialExpanded.setVisibility(View.VISIBLE);
+//                close.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        socialExpanded.setVisibility(View.GONE);
+////                        white.setVisibility(View.INVISIBLE);
+//                    }
+//                });
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("PlaceholderFragment", "onClick: facebook call");
+                startActivity(newFacebookIntent(context.getPackageManager(),fbIds[position]));
             }
         });
 
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                instagram();
+            }
+        });
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(websites[position]));
+                startActivity(intent);
+            }
+        });
         aboutLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -124,6 +157,32 @@ public class PlaceholderFragment extends Fragment {
         });
 
         return root;
+    }
+
+    public static Intent newFacebookIntent(PackageManager pm, String url) {
+        Uri uri = Uri.parse(url);
+        try {
+            Log.d("PlaceholderFragment", "newFacebookIntent: facebook intent called");
+            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
+            if (applicationInfo.enabled) {
+                uri = Uri.parse("fb://page/"+url);
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+            uri = Uri.parse("https://www.facebook.com/"+url);
+        }
+        return new Intent(Intent.ACTION_VIEW,uri);
+    }
+    public void instagram(){
+        Uri uri = Uri.parse("http://instagram.com/_u/bvpieee");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/bvpieee")));
+        }
     }
     @Override
     public void onAttach(@NonNull Context context) {
